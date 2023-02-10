6e75377399bb28b823d74a6e419efabc7d01485e
GeekanskiY <astrovskiy.04@gmail.com>
2023-02-10 15:29:25 +0300

    types init

--- src/components/calculator/Display.tsx
+++ src/components/calculator/Display.tsx
@@ -1,1 +1,12 @@
-import React from "react";
+import React, {FunctionComponent} from "react";
+
+type display = {
+    "value1": Number,
+    "value2": Number,
+    "result": Number,
+    "operation": String,
+}
+
+export const display:FunctionComponent = () => {
+    return <div></div>
+}
