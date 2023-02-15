41047fd3645437a92f4c445d339a6ef949c88170
GeekanskiY <astrovskiy.04@gmail.com>
2023-02-15 13:11:02 +0300

    display init

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent } from 'react';
+import { Display } from './Display';
+
 
 type CalcProps = {
     value1: Number,
     value2: Number,
-    result: Number
+    result: Number,
+
+    num_buttons: [FunctionComponent]
   }
 
 
   export const Calculator: FunctionComponent<CalcProps> = ({value1=null, value2=null, result=null}) => {
-    return <div>asd</div>
+    
+
+    return <div className='calculator'>
+      <Display value1={1} value2={2} result={3} operation={"+"}/>
+    </div>
   }
