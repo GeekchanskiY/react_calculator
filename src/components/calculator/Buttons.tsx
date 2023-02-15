1081b89f5c9da3852f34f74d60bac4bebd943085
GeekanskiY <astrovskiy.04@gmail.com>
2023-02-15 13:43:07 +0300

    numeric buttons init

--- src/components/calculator/Buttons.tsx
+++ src/components/calculator/Buttons.tsx
@@ -18,20 +18,35 @@ class AddCommand extends Command {
     }
 }
 
+
+// size from 1 to 3 - widget display size
+// isFunctional - True for numeric buttons, False for functional
 type ButtonType = {
-    "size": String,
+    "size": Number,
     "isFunctional": boolean
 }
 
-type ButtonProps = {
+export type ButtonProps = {
     "value": Number,
     "buttonType": ButtonType,
     "displayValue": String,
-    "callbackFunction": (value: Number, buttonType: ButtonType) => null
+    "callbackF": (value: Number, buttonType: ButtonType) => null
 }
 
-export const Button:FunctionComponent<ButtonProps> = ({value, buttonType, displayValue, callbackFunction}) => {
+
+
+export const Button:FunctionComponent<ButtonProps> = ({value, buttonType, displayValue, callbackF}) => {
     return <div className="button">
         {displayValue}
     </div>
+}
+
+export const Buttons:FunctionComponent = () => {
+    const numeric_button_type: ButtonType = {size: 1, isFunctional: false}  
+    let i: Number, el: String;
+    let numeric_buttons_data: ButtonProps[] = []
+    "123456789".split('').map((el, i) => {numeric_buttons_data.push({value: Number(el), buttonType: numeric_button_type, displayValue: el, callbackF: (value, buttonType) => {return null}})} )
+    console.log(numeric_buttons_data)
+
+    return <div></div>
 }
